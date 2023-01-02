4e74195642c892c5c8a0bf848c2d7f6b39599317
zabieru363 <zabierujlc@gmail.com>
2023-01-02 18:36:32 +0100

    El campo degree se comporta correctamente

--- js/test.js
+++ js/test.js
@@ -11,6 +11,22 @@
     }
 
     // Creando objeto Student:
-    const student = new Student("Javier", "05722054E", new Date(2000, 8, 6), "others", 7);
+    const student = new Student("Javier", "05722054E", new Date(2000, 8, 6), "vocacional", 7);
     console.log(student);
+
+    // Si el DNI no es válido arroja un error:
+    try {
+        const s1 = new Student("Javier", "05722", new Date(2000, 8, 6), "others", 7);
+        console.log(s1);
+    }catch(error) {
+        console.error(error);
+    }
+    
+    // Solo se pueden elegir 3 modalidades de estudios:
+    try {
+        const s2 = new Student("Javier", "05722054E", new Date(2000, 8, 6), "unknown", 7);
+        console.log(s2);
+    }catch(error) {
+        console.error(error);
+    }
 })();
