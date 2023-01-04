ce7745cb5a1c50d44176dd5b2edfa2c69487df85
zabieru363 <zabierujlc@gmail.com>
2023-01-04 20:40:04 +0100

    Método generador de cursos creado en clase HighSchool

--- js/entities.js
+++ js/entities.js
@@ -170,4 +170,10 @@ class HighSchool {
 
         this.#courses.splice(pos, 1);
     }
+
+    * courses() {
+        for(const course of this.#courses) {
+            yield course.name;
+        }
+    }
 }
