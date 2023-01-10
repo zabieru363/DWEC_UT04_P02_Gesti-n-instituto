81b5e9e642ddfed5c576f630569e609bba2d713b
zabieru363 <zabierujlc@gmail.com>
2023-01-10 05:12:21 +0100

    Objeto List añadido a la práctica

--- js/exceptions.js
+++ js/exceptions.js
@@ -141,3 +141,40 @@ class CourseIsFullException extends BaseException {
     this.name = "CourseIsFullException";
   }
 }
+
+// * EXCEPCIONES OBJETO LIST
+
+/**
+ * Clase de excepción personalizada para mostrar un
+ * mensaje de error en el case de que la lista esté llena.
+ */
+class ListIsFullException extends BaseException {
+  constructor(fileName, lineNumber) {
+      super("La lista está llena.", fileName, lineNumber);
+      this.name = "ListIsFullException";
+  }
+}
+
+/**
+* Clase de excepción personalizada para mostrar un
+* mensaje de error en el case de que la lista esté vacía.
+*/
+class ListIsEmptyException extends BaseException {
+  constructor(fileName, lineNumber) {
+      super("La lista está vacía.", fileName, lineNumber);
+      this.name = "ListIsEmptyException";
+  }
+}
+
+/**
+* Clase de excepción personalizada para mostrar un
+* mensaje de error en el case de que los limites de
+* la capacidad de la lista hayan sido superados a la hora
+* de invocar métodos que trabajen con los index.
+*/
+class IndexOutOfBoundsListException extends BaseException {
+  constructor(fileName, lineNumber) {
+      super("El indice está fuera de los limites de la lista.", fileName, lineNumber);
+      this.name = "IndexOutOfBoundsListException";
+  }
+}
